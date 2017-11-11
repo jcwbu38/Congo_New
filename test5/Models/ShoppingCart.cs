9dc5f67a924a56a45220194df7bc3919394b972b
Chappel, Jon <jchap4@uisad.uis.edu>
2017-11-11 16:10:45 -0700

    Added shopping cart page and supporting classes.

---
+++ test5/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test5.Models.ShoppingCart
+{
+    public class Cart
+    {
+        public string image { set; get; }
+        public string description { set; get; }
+        public string price { set; get; }
+        public string discountPrice { set; get; }
+        public DateTime EntryDate;
+    }
+}
