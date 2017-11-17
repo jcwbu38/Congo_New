1f35bc194593b77443e2716482c2072854898575
Derek <derekcoppinger@gmail.com>
2017-11-16 21:23:15 -0600

    Creating more ViewModels - CheckoutViewModel - HomeViewModel - ProductDetailsViewModel

---
+++ test5/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test5.ViewModels
+{
+    public class HomeViewModel
+    {
+    }
+}
