87111402f99afab93bb573cff691f34ce0f41eac
Derek <derekcoppinger@gmail.com>
2017-11-26 18:00:17 -0600

    Adding Phone an UserType Properties to User Model and migrated it to the database

--- test5/Models/User.cs
+++ test5/Models/User.cs
@@ -5,19 +5,33 @@ namespace test5.Models
     public class User
     {     
         public int Id { get; set; }
+
+        public string UserType { get; set; }
+
         [Required]
         [StringLength(100)]
         public string First { get; set; }
+
         [Required]
         [StringLength(100)]
         public string Last { get; set; }
+
         public string Address1 { get; set; }
+
         public string Address2 { get; set; }
+
         [StringLength(2)]
         public string State { get; set; }
+
         [MaxLength(5)]
         public int Zip { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
         [EmailAddress]
         public string Email { get; set; }
+
+        
     }
 }
